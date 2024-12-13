9b9bf9d3a6f3cf3a1a167cf705641dd8d1e5fc68
Ko sungbin <kosb7544@gmail.com>
2024-12-13 19:21:03 +0900

    remove base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://jagajang.github.io',
-	base: '/astro-blog',
-	integrations: [mdx(), sitemap()]
+  site: 'https://jagajang.github.io',
+  integrations: [mdx(), sitemap()],
 });
